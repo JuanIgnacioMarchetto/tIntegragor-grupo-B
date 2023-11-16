620b30405458da51e582376accd36b389bd19b7a
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-16 11:16:49 -0300

    16/11/2023-11:11

--- src/components/taskItem/taskIten.jsx
+++ src/components/taskItem/taskIten.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
  import './TaskItem.css'
 
-const TaskItem = ({description, complete}) => {
-
-    complete=true
+const TaskItem = ({description}) => {
+  const [completed, setCompleted]= useState(false);
+  const handleComplete=()=>{
+    setCompleted(!completed);
+  }
 
   return (
-    <div className='item-container'><p className={complete ?'taskComplete':'task-item'}>{description}</p>
-    <button type='button'>x</button>
+    <div className='item-container'>
+      <p className={completed ? 'taskComplete' : 'task-item'}>{description}</p>
+      <button type='button' onClick={handleComplete}>
+        Completar
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default TaskItem
