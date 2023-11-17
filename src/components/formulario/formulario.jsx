9c984146fb5ce4e69a0e9c62cd712ed8a916403a
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-17 10:26:08 -0300

    17/11/2023/8:26

--- src/components/formulario/formulario.jsx
+++ src/components/formulario/formulario.jsx
@@ -6,6 +6,7 @@ import 'react-datetime/css/react-datetime.css';
 const TaskForm = () => {
     const [taskDescription, setTaskDescription] = useState('');
     const [taskDeadline, setTaskDeadline] = useState('');
+    const [tasks, setTasks] = useState([]);
 
     const handleDescriptionChange = (e) => {
         setTaskDescription(e.target.value);
@@ -18,27 +19,41 @@ const TaskForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const newTask = {
-        description: taskDescription,
-            deadline: taskDeadline
-    };
-    console.log(newTask);
-    setTaskDescription('');
-    setTaskDeadline('');
-};
+            description: taskDescription,
+            deadline: taskDeadline ? taskDeadline.format('YYYY-MM-DD HH:mm:ss') : ''
+        };
 
-return (
-    <form onSubmit={handleSubmit}>
-        <div>
-            <label>Descripción de la tarea:</label>
-            <input type="text" value={taskDescription} onChange={handleDescriptionChange} />
-        </div>
-        <div>
-            <label>Fecha límite:</label>
-            <Datetime value={taskDeadline} onChange={handleDeadlineChange} />
-        </div>
-        <button type="submit">Agregar tarea</button>
-    </form>
-);
-   };
+        setTasks([...tasks, newTask]);
+        setTaskDescription('');
+        setTaskDeadline('');
+    };
+
+    const handleConfirmTask=(index)=>{console.log('Tarea realizada:$tasks.[index]')}
+    const handleDeleteTask=(index)=>{const updatedTasks=[...tasks];updatedTasks.splice(index,1);setTasks(updatedTasks)}
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Descripción de la tarea:</label>
+                <input type="text" value={taskDescription} onChange={handleDescriptionChange} />
+            </div>
+            <div>
+                <label>Fecha límite:</label>
+                <Datetime value={taskDeadline} onChange={handleDeadlineChange} />
+            </div>
+            <button type="submit">Agregar tarea</button>
+
+            { }
+            {tasks.map((task, index) => (
+                <div key={index}>
+                    <p>Tu tarea:</p>
+                    <p>Descripción: {task.description}</p>
+                    <p>Fecha límite: {task.deadline}</p>
+                    <button onClick={() => handleDeleteTask(index)}>Eliminar tarea</button>
+                    <button onClick={() => handleConfirmTask(index)}>Confirmar tarea</button>
+                </div>
+            ))}
+        </form>
+    );
+};
 
 export default TaskForm;
