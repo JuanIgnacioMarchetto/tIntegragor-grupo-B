eecd0d11907e96ca3a4d8a1d131099117d2378bd
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-17 09:38:45 -0300

    17/11/2023/8:26

--- src/components/formulario/formulario.jsx
+++ src/components/formulario/formulario.jsx
@@ -3,41 +3,41 @@ import './formulario.css';
 import Datetime from'react-datetime.css';
 import 'react-datetime/css/react-datetime.css'
 
+
 const TaskForm = ({ onAddTask }) => {
-    const [task, setTask] = useState('');
-    const handleInputChange = (e) => {
-        setTask(e.target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (task.trim() === '') {
-            return;
-        }
-        onAddTask(task);
-        setTask('');
-    };
-    return (
+ const [task, setTask] = useState('');
+ const [selectedDate, setSelectedDate] = useState(null);
 
-        <form className='task-form' onSubmit={handleSubmit}>
-            <div className="conteinerform">
-                <label htmlFor="taskInput" className="form-label">
-                    Nueva Tarea
-                </label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="taskInput"
-                    value={task}
-                    onChange={handleInputChange}
-                />
-            </div>
-            <button type="submit" className="btn Add">
-                Agregar Tarea
-            </button>
-        </form>
-    );
+ const handleInputChange = (e) => {
+ setTask(e.target.value);
+ };
+
+ const handleDateChange = (date) => {
+ setSelectedDate(date);
+ };
+
+ const handleSubmit = (e) => {
+ e.preventDefault();
+ if (task.trim() !== '') {
+ onAddTask(task, selectedDate);
+ setTask('');
+ setSelectedDate(null);
+ }
+ };
+
+ return (
+ <form onSubmit={handleSubmit}>
+ <label htmlFor="taskInput">Task:</label>
+ <input type="text"
+ id="taskInput"
+ value={task}
+ onChange={handleInputChange}
+ />
+ <label>Date and Time:</label>
+ <Datetime value={selectedDate} onChange={handleDateChange} />
+ <button type="submit">Add Task</button>
+ </form>
+ );
 };
-
-
 
 export default TaskForm;
