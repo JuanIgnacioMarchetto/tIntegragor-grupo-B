b1e201c07cfdb66bdcace0804e52bfe85df9a212
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-17 09:50:26 -0300

    17/11/2023/8:26

--- src/components/formulario/formulario.jsx
+++ src/components/formulario/formulario.jsx
@@ -1,43 +1,47 @@
 import React, { useState } from 'react';
-import './formulario.css'; 
-import Datetime from'react-datetime.css';
-import 'react-datetime/css/react-datetime.css'
-
+import './formulario.css';
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
 
 const TaskForm = ({ onAddTask }) => {
- const [task, setTask] = useState('');
- const [selectedDate, setSelectedDate] = useState(null);
+    const [task, setTask] = useState('');
+    const [selectedDate, setSelectedDate] = useState(null);
 
- const handleInputChange = (e) => {
- setTask(e.target.value);
- };
+    const handleInputChange = (e) => {
+        setTask(e.target.value);
+    };
 
- const handleDateChange = (date) => {
- setSelectedDate(date);
- };
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
 
- const handleSubmit = (e) => {
- e.preventDefault();
- if (task.trim() !== '') {
- onAddTask(task, selectedDate);
- setTask('');
- setSelectedDate(null);
- }
- };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task.trim() !== '') {
+            onAddTask(task, selectedDate);
+            setTask('');
+            setSelectedDate(null);
+        }
+    };
 
- return (
- <form onSubmit={handleSubmit}>
- <label htmlFor="taskInput">Task:</label>
- <input type="text"
- id="taskInput"
- value={task}
- onChange={handleInputChange}
- />
- <label>Date and Time:</label>
- <Datetime value={selectedDate} onChange={handleDateChange} />
- <button type="submit">Add Task</button>
- </form>
- );
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="taskInput">Task:</label>
+            <input
+                type="text"
+                id="taskInput"
+                value={task}
+                onChange={handleInputChange}
+            />
+            <label>Date and Time:</label>
+            <Datetime
+                inputProps={{ id: 'datetimeInput' }}
+                value={selectedDate}
+                onChange={handleDateChange}
+            />
+            <button type="submit">Add Task</button>
+        </form>
+    );
 };
 
 export default TaskForm;
