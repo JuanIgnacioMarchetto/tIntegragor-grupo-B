3b0a3041c903504dae715f347397084d7eb140f1
Fernando <fernando.pereyra764@gmail.com>
2023-11-17 13:02:07 -0300

    cambios del main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import TaskForm from './components/formulario';
-<<<<<<< HEAD
+import TaskForm from './components/Formulario';
 import TaskItem from './components/TaskItem/TaskItem';
-=======
-import TaskItem from './components/taskItem/taskIten';
->>>>>>> main
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -38,20 +34,9 @@ const App = () => {
     <div className="container">
       <h1>To Do List</h1>
       <TaskForm onAddTask={handleAddTask} />
-<<<<<<< HEAD
       <ul className="list-group mt-3">
         {tasks.map((task, index) => (
           <TaskItem  key={index} className="border border-dark list-group-item" description={task}/>
-=======
-      <ul className="list-group">
-        {tasks.map((task) => (
-          <TaskItem
-            key={task.id}
-            task={task}
-            onCompleteTask={handleCompleteTask}
-            onDeleteTask={handleDeleteTask}
-          />
->>>>>>> main
         ))}
       </ul>
     </div>
